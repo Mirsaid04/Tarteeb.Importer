0ab9ab6000e6856789b694023d116e1b0b6eeace
Mirsaid04 <mirsaidd04@gmail.com>
2023-09-20 23:00:37 +0900

    DATA: Model and Migrate Clients Table

---
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,25 @@
+﻿//=========================
+// Copyrite (c) Tarteeb LLC 
+// Powering True Leadership
+//=========================
+
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Tarteeb.Importer.Models.Clients;
+
+namespace Tarteeb.Importer.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public StorageBroker() =>
+            this.Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source = ..\\..\\..\\Tarteeb.db";
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
