715d031720bb9dc07144e93ba1da588e4edad444
Mirsaid04 <mirsaidd04@gmail.com>
2023-09-22 18:48:14 +0900

    BROKERS: Insert Client

--- Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -4,6 +4,7 @@
 //=========================
 
 
+using System.Threading.Tasks;
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Tarteeb.Importer.Models.Clients;
@@ -16,6 +17,13 @@ namespace Tarteeb.Importer.Brokers.Storages
         public StorageBroker() =>
             this.Database.EnsureCreated();
 
+        public async Task<Client> InsertClientAsync (Client client)
+        {
+            await this.Clients.AddAsync(client);
+            await this.SaveChangesAsync();
+
+            return client;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Data Source = ..\\..\\..\\Tarteeb.db";
