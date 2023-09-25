e6487d198b8957289c4b19b41c35f00e3c734e56
Mirsaid04 <mirsaidd04@gmail.com>
2023-09-25 19:28:18 +0900

    BROKERS: Create Logging Broker

---
+++ Tarteeb.Importer/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,18 @@
+﻿//=========================
+// Copyrite (c) Tarteeb LLC 
+// Powering True Leadership
+//=========================
+
+
+using System;
+
+namespace Tarteeb.Importer.Brokers.Loggings
+{
+    internal class LoggingBroker
+    {
+        public void LogError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
