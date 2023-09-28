1f2233e07f6c847d53ac5a27947cc1e2167d73be
Mirsaid04 <mirsaidd04@gmail.com>
2023-09-28 18:08:53 +0900

    POC: Handle Exception

---
+++ Tarteeb.Importer/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarteeb.Importer.Brokers.DateTimes
+{
+    internal class DateTimeBroker
+    {
+       internal DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ Tarteeb.Importer/Services/Clients/ClientService.Exeptions.cs
@@ -0,0 +1,39 @@
+﻿//=========================
+// Copyrite (c) Tarteeb LLC 
+// Powering True Leadership
+//=========================
+
+
+using System.Threading.Tasks;
+using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Clients.Exceptions;
+
+namespace Tarteeb.Importer.Services.Clients
+{
+    internal partial class ClientService
+    {
+        private delegate Task<Client> ReturningClientFunction();
+
+        private Task<Client> TryCatch(ReturningClientFunction returningClientFunction)
+        {
+            try
+            {
+                return returningClientFunction();
+            }
+            catch (NullClientException nullClientException)
+            {
+                var clientValidationException =
+                     new ClientValidationException(nullClientException);
+
+                throw clientValidationException;
+            }
+            catch (InvalidClientException invalidClientException)
+            {
+                var clientValidationExceptiont =
+                    new ClientValidationException(invalidClientException);
+
+                throw clientValidationExceptiont;
+            }
+        }
+    }
+}
